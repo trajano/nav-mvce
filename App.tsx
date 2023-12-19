11c7e7211663e763030d9cedaa89c49d68ce5c46
Archimedes Trajano <developer@trajano.net>
2023-12-18 23:23:50 -0500

    initial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, View } from 'react-native';
+
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { NativeStackScreenProps, createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Button, Icon, Text } from '@rneui/themed';
+import { useCallback } from 'react';
+
+type RootStackParamList = {
+  Home: undefined;
+  Settings: undefined;
+};
+type RootStackScreenProps<T extends keyof RootStackParamList> =
+  NativeStackScreenProps<RootStackParamList, T>;
+const HomeScreen = ({ navigation }: RootStackScreenProps<"Home">) => {
+  const showSettings = useCallback(() => { navigation.navigate("Settings") }, [navigation]);
+  return <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}><Button onPress={showSettings}>show settings</Button></View>
+}
+
+const SettingsScreen = ({ navigation }: RootStackScreenProps<"Settings">) => {
+  return <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}><Text>Settings</Text></View>
+}
+
+const RootStack = createNativeStackNavigator<RootStackParamList>();
+const RootNavigator = () => {
+  const navigation = useNavigation();
+  const headerRight = useCallback(() => (<Pressable onPress={() => { navigation.goBack() }}><Icon name="close" /></Pressable >), [navigation]);
+  return (<RootStack.Navigator screenOptions={{ headerShown: false }}>
+    <RootStack.Screen name="Home" component={HomeScreen} />
+    <RootStack.Screen name="Settings" component={SettingsScreen}
+      options={{
+        presentation: "modal",
+        animation: "slide_from_bottom",
+        headerShown: true,
+        headerRight: headerRight
+      }} />
+  </RootStack.Navigator>)
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootNavigator />
+      <StatusBar style='auto' />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
